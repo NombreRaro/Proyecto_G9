cd0dbb98210de5fb3eb6b5df8841c989da28383a
NombreRaro <145030957+NombreRaro@users.noreply.github.com>
2023-11-14 23:04:31 +0100

    Version 4 del proyecto

---
+++ git/projecte/ejercicio individual/ejercicio individual/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ejercicio_individual
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        Thread atender;
+        int codigo;
+        string nombre, mssg, altura;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            button1.BackColor = Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress newaddress = IPAddress.Parse("192.168.145.138");
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint remoteEP = new IPEndPoint(newaddress, 9050);
+            try
+            {
+                server.Connect(remoteEP);
+                MessageBox.Show("Conectado");
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show("Conectado");
+                Console.WriteLine("No se ha podido conectar al servidor");
+                Console.WriteLine(ex);
+                return;
+            }
+            button1.BackColor = Color.Green;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.codigo = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.codigo = 2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.nombre = textBox1.Text;
+            this.altura = textBox2.Text;
+            if (textBox2.Text==null)
+            {
+                this.altura = "";
+            }
+
+            if (codigo == 1)
+            {
+                this.mssg = "1/" + this.nombre + "/" + this.altura;
+                byte[] m = System.Text.Encoding.ASCII.GetBytes(mssg);
+                server.Send(m);
+                //Rebem la resposta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mssg = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show("La longitud del nombre es: "+this.mssg);
+            }
+            else
+            {
+                this.mssg = "2/" + this.nombre + "/" + this.altura;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(mssg);
+                server.Send(mensaje);
+                //Rebem la resposta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mssg = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                if (mssg == "SI")
+                {
+                    MessageBox.Show("Tu nombre es bonito");
+                }
+                else
+                {
+                    MessageBox.Show("Tu nombre no es bonito");
+                }
+            }
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
---
+++ git/projecte/ejercicio individual/ejercicio individual/Form2.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ejercicio_individual
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Registre = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Location = new System.Drawing.Point(127, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Password";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(332, 91);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 51);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "ENVIAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 95);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Username";
+            // 
+            // Registre
+            // 
+            this.Registre.AutoSize = true;
+            this.Registre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Registre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registre.Location = new System.Drawing.Point(12, 9);
+            this.Registre.Name = "Registre";
+            this.Registre.Size = new System.Drawing.Size(84, 27);
+            this.Registre.TabIndex = 17;
+            this.Registre.Text = "LOG IN";
+            this.Registre.Click += new System.EventHandler(this.Registre_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(206, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(494, 176);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Registre);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Registre;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
